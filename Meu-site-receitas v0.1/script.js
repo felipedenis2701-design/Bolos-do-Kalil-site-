2dcef46a4fcf624a595d6d025ab090e3d91ad34d
Felipe Kalil <felipedenis2701@gmail.com>
2026-06-26 11:48:06 -0300

    Add files via upload

---
+++ Meu-site-receitas v0.1/script.js
@@ -0,0 +1,8 @@
+function mostrarMensagem(nomeReceita) {
+    alert("Que legal! Você gostou da receita: "+ nomeReceita);
+    }
+
+    function mudarCor() {
+    document.body.style.backgroundColor = "#ffe0b2";
+    }
+
